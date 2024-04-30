19aac77d36661193e61abd26eabe8070de63305e
MatthewRGhost <68789322+matthewnex12@users.noreply.github.com>
2024-04-29 22:10:08 -0700

    V1
    Version 1 of the project

---
+++ Authentication.h
@@ -0,0 +1,15 @@
+/* 
+ * File:   Authentication.h
+ * Author: mattr
+ *
+ * Created on April 29, 2024, 9:47 PM
+ */
+
+#ifndef AUTHENTICATION_H
+#define AUTHENTICATION_H
+
+void loginUser();
+void registerUser();
+
+#endif /* AUTHENTICATION_H */
+
---
+++ Menu.h
@@ -0,0 +1,18 @@
+/* 
+ * File:   Menu.h
+ * Author: Matthew Rodriguez
+ *
+ * Created on April 29, 2024, 2:45 PM
+ */
+
+#ifndef MENU_H
+#define MENU_H
+
+void displayMainMenu();
+void displayUserMenu();
+void displayAdminMenu();
+void takeSurvey();
+void displayAdminMenu();
+
+#endif
+
